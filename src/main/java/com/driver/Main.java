9c95f9e74a6d91c6abc18ee0fb29351dc1828145
Anantjain1004 <jainanant895@gmail.com>
2023-01-13 17:49:05 +0530

    Second

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,12 +3,11 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B obj = new B();
-        System.out.println(obj.meth());
+        obj.meth();
 
     }
 }
 class A{
-
     public String meth(){
         return "Invoking method from class A";
     }
